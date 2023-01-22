270f59ca43322bb431215a0c926fc2d158e42fd6
Abdelhadi Amhamdi <aamhamdi@e2r8p5.1337.ma>
2023-01-22 21:01:47 +0100

    [fix] integer overflow

--- error_handling.c
+++ error_handling.c
@@ -6,42 +6,52 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 14:05:51 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/22 01:00:40 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/01/22 20:23:33 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft/libft.h"
 #include "push_swap.h"
+
+int	check_empty_arg(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] != ' ')
+			return (0);
+		index++;
+	}
+	return (1);
+}
 
 char	*join_all_args(char **av, int ac)
 {
 	int		index ;
 	char	*data ;
-	int j;
+	int		j;
 
 	index = 1;
 	data = ft_calloc(sizeof(char), 1);
 	while (index < ac)
 	{
 		j = 0;
-		if(!av[index][0])
+		if (!av[index][0] || av[index][0] == ' ')
 		{
-			printf("Error\n");
-			return 0;
+			if ((av[index][0] == ' ' && check_empty_arg(av[index])) || \
+			!av[index][0])
+				return (printf("Error\n"), NULL);
 		}
 		while (av[index][j] != '\0')
 		{
-			if (!ft_isdigit(av[index][j]) && (av[index][j] != '-' && av[index][j] != ' '))
-			{
-				// free_all(tabs);
-				printf("Error\n");
-				return (0);
-			}
-			if(av[index][j] == '-' && (!av[index][j+1] || av[index][j+1] == ' '))
-			{
-				printf("Error\n");
-				return 0;
-			}
+			if ((!ft_isdigit(av[index][j]) && av[index][j] != '-') && \
+			(av[index][j] != ' '))
+				return (printf("Error\n"), NULL);
+			if (av[index][j] == '-' && \
+			(!av[index][j + 1] || av[index][j + 1] == ' '))
+				return (printf("Error\n"), NULL);
 			j++;
 		}
 		data = ft_strjoin(data, av[index]);
@@ -50,22 +60,12 @@ char	*join_all_args(char **av, int ac)
 	return (data);
 }
 
-size_t	ft_lenght(char **tabs)
-{
-	size_t	index;
-
-	index = 0 ;
-	while (tabs[index])
-		index++;
-	return (index);
-}
-
 int	*filter_args(char **tabs, size_t size)
 {
 	int	index;
-	// int	j;
 	int	*data;
 	int	num;
+	int	sign;
 
 	index = 0 ;
 	data = ft_calloc(size, sizeof(int));
@@ -73,28 +73,21 @@ int	*filter_args(char **tabs, size_t size)
 		return (NULL);
 	while (tabs[index])
 	{
-		// j = 0;
-		// while (tabs[index][j] != '\0')
-		// {
-		// 	if (!ft_isdigit(tabs[index][j]) && tabs[index][j] != '-')
-		// 	{
-		// 		// free_all(tabs);
-		// 		printf("Error\n");
-		// 		return (NULL);
-		// 	}
-		// 	j++;
-		// }
 		num = ft_atoi(tabs[index]);
-		if (num >= 2147483647 || num <= -2147483648)
+		sign = check_sign(tabs[index]);
+		if ((sign < 0 && num > 0) || (sign > 0 && num < 0) || \
+		(ft_strlen(tabs[index]) > 11))
 		{
 			printf("Error\n");
-			return (free(data), NULL);
+			free(data);
+			return (NULL);
 		}
 		data[index] = num;
 		if (!check_deplucate(data, num, index))
 		{
 			printf("Error\n");
-			return (free(data), NULL);
+			free(data);
+			return (NULL);
 		}
 		free(tabs[index]);
 		index++;
