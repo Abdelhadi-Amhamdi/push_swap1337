ad7b01cd1b23470a3da536284ee9f80b6e967b6d
Abdelhadi Amhamdi <aamhamdi@e2r9p2.1337.ma>
2023-01-22 16:23:12 +0100

    [fix] norminette

--- push_swap.c
+++ push_swap.c
@@ -6,53 +6,40 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 09:08:56 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/22 00:34:07 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/01/22 16:22:10 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
 
-
-void print_list(t_list *list)
+int	*parsing(char **av, int ac, int *size)
 {
-	int top;
-	t_list *tmp;
+	char	*args_data;
+	char	**tabs;
 
-	if(list)
-	{
-		top = list->list_data;
-		tmp = list;
-		while(tmp)
-		{
-			printf("value: %d -- index: %d -- less: %d -- isl: %d\n", tmp->list_data, tmp->index, tmp->less_than, tmp->is_longest);
-			tmp = tmp->next;
-			if(tmp->list_data == top)
-				break;
-		}
-	}
-	
+	args_data = join_all_args(av, ac);
+	if (!args_data)
+		return (0);
+	tabs = ft_split(args_data, ' ');
+	if (!tabs)
+		return (0);
+	free(args_data);
+	*size = ft_lenght(tabs);
+	return (filter_args(tabs, *size));
 }
 
 int	main(int ac, char **av)
 {
 	t_stack	a;
 	t_stack	b;
-	char	*args_data;
-	char	**tabs;
 	int		*data;
 	size_t	size;
 
 	if (ac > 1)
 	{
-		args_data = join_all_args(av, ac);
-		if(!args_data)
-			return 0;
-		tabs = ft_split(args_data, ' ');
-		free(args_data);
-		size = ft_lenght(tabs);
-		data = filter_args(tabs, size);
-		if (data == NULL)
+		data = parsing(av, ac, &size);
+		if (!data)
 			return (0);
 		if (!(init_stack(size, data, &a, &b)))
 			return (0);
@@ -64,7 +51,7 @@ int	main(int ac, char **av)
 				sort_five(&a, &b);
 			else
 				sort_algo(&a, &b);
-		}	
+		}
 	}
 	return (0);
 }
